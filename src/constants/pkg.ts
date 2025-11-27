c30f77e89bf58a7807ed4a47c7d6bfd56e80aed2
DevAbabil <ababilhossain35@gmail.com>
2025-11-27 23:30:56 +0600

    chore(pkg): bump version to 1.0.4

--- src/constants/pkg.ts
+++ src/constants/pkg.ts
@@ -6,7 +6,7 @@ interface PackageJson {
 
 const pkg: PackageJson = {
   name: 'jobx',
-  version: '1.0.3',
+  version: '1.0.4',
   description:
     'AI-powered CLI tool to automate job applications, generate personalized emails, and track applications using Google Sheets.',
 };
